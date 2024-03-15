ce7ec31860bc5ebac836a2a3c7407cfa97d20645
I-am-ashok <mashok4102@gmail.com>
2024-03-15 15:04:17 +0530

    removeing files from master branch from gitcmd

--- src/main/python/bin/SparkObject.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-
-def sessionobject(v_master, v_appname,sparkobjectlogger):
-    sparkobjectlogger.info("SPark Session is creating...")
-    spark = SparkSession.builder.master(v_master).appName(v_appname).getOrCreate()
-    sparkobjectlogger.info("spark session has been created ...")
-    return spark
