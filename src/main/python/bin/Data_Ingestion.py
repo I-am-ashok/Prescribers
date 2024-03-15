478987baed3e1c4ea9f8e536fa10ab90b9eb3bb8
I-am-ashok <mashok4102@gmail.com>
2024-03-15 15:11:43 +0530

    removeing files from master branch from gitcmd

--- src/main/python/bin/Data_Ingestion.py
+++
@@ -1,9 +0,0 @@
-def dimensiondf(spark,dimensionfile,Factfile,Ingesionlogger):
-    Ingesionlogger.info("spark data loading is in progress .......")
-    dimension = spark.read.parquet(dimensionfile)
-    fact = spark.read.csv(Factfile, header=True, inferSchema=True)
-    Ingesionlogger.info("spark dataframes have been created ..")
-    return dimension, fact
-
-
-
--- src/main/python/bin/main.py
+++
@@ -1,9 +0,0 @@
-from objects import *
-
-if __name__ == '__main__':
-    mainlogger.info(f"Dimension file count is {dimension_df.count()}")
-    mainlogger.info(f"Fact file count is {fact_df.count()}")
-    mainlogger.info("the 2 dataframes have been loaded successfully!!!")
-    mainlogger.info("Preprocessing script is calling!!")
-    Pre_processing_df = Pre_processing_df.limit(2).toPandas()
-    mainlogger.info("The Preprocessing dataframe is \n"+Pre_processing_df.to_string())
